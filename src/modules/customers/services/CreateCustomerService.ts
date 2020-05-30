181e87cb91bf22eb8fad35b55b387ca94609c37f
leonardosaraivaota <leonardosaraivaota@gmail.com>
2020-05-29 21:37:45 -0300

    First Commit

---
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -0,0 +1,35 @@
+import { inject, injectable } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+
+import Customer from '../infra/typeorm/entities/Customer';
+import ICustomersRepository from '../repositories/ICustomersRepository';
+
+interface IRequest {
+  name: string;
+  email: string;
+}
+
+@injectable()
+class CreateCustomerService {
+  constructor(
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) {}
+
+  public async execute({ name, email }: IRequest): Promise<Customer> {
+    const findCustomer = await this.customersRepository.findByEmail(email);
+    if (findCustomer) {
+      throw new AppError('User already exists');
+    }
+
+    const customer = await this.customersRepository.create({
+      name,
+      email,
+    });
+
+    return customer;
+  }
+}
+
+export default CreateCustomerService;
---
+++ src/shared/infra/typeorm/migrations/1590620194860-CreateOrders.ts
@@ -0,0 +1,76 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateOrders1590620194860 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'orders',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          /*
+          {
+            name: 'product_id',
+            type: 'uuid',
+          },
+          */
+          {
+            name: 'customer_id',
+            type: 'uuid',
+          },
+          /*
+          {
+            name: 'price',
+            type: 'decimal',
+            precision: 10,
+            scale: 2,
+          },
+          {
+            name: 'quantity',
+            type: 'integer',
+          },
+           */
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+        foreignKeys: [
+          /*
+          {
+            name: 'OrderProductId',
+            referencedTableName: 'products',
+            referencedColumnNames: ['id'],
+            columnNames: ['product_id'],
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE',
+          },
+          */
+          {
+            name: 'OrderCustomerId',
+            referencedTableName: 'customers',
+            referencedColumnNames: ['id'],
+            columnNames: ['customer_id'],
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('orders');
+  }
+}
